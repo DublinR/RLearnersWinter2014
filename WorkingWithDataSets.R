31c17e7493613888987e6a8b668d1f01940d3019
Dublin R <DublinRUserGroup@gmail.com>
2014-08-12 13:51:08 -0700

    Update WorkingWithDataSets.R

--- WorkingWithDataSets.R
+++ WorkingWithDataSets.R
@@ -11,6 +11,9 @@ head(idahoData)[,1:10]
 
 summary(idahoData[,1:10])
 
+##############################################################################################
+
+
 table(idahoData$TYPE)
 names(idahoData)
 summary(idahoData[,11:30])
@@ -22,3 +25,11 @@ str(idahoData[,11:50])
 summary(idahoData$ST)
 # How many housing units [are] worth more than $1,000,000?
 table(idahoData$TYPE,idahoData$VAL)
+
+##############################################################################################
+
+# How many households have 3 bedrooms and and 4 total rooms?
+# How many households have 2 bedrooms and 5 total rooms?
+# How many households have 2 bedrooms and 7 total rooms?
+
+table(idahoData$RMS,idahoData$BDS,dnn=list("RMS","BDS"))
