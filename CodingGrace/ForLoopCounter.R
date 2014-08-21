52ecbb996a1b4f161722979961bdba50a9de1b21
Dublin R <DublinRUserGroup@gmail.com>
2014-08-21 11:05:21 +0100

    Update ForLoopCounter.R

--- CodingGrace/ForLoopCounter.R
+++ CodingGrace/ForLoopCounter.R
@@ -1,12 +1,14 @@
 
-for(i in 1:10000)
-{
+LEN = nrow(MASTER)
+Over12s = numeric()
 
-Milestones=c(150,300,500,1000,1500,2000,3000,5000,7000,9000)
+for(i in 1:LEN){
 
-if(i %in% Milestones){
-	cat("\n",format(Sys.time(), "%X "),"| i is",i,"| \n")
+if( i %in% c(500,1000,2000,3500,5000,7000) ){
+ cat("\n Reached Iteration :",i," out of",LEN," Iterations")
+ if (.Platform$OS.type == "windows") flush.console()
 
-	}
-
+ }
+TEMP=MASTER[i,]
+Over12s= c(Over12s,length(which(TEMP[,grep("years",names(MASTER))]>12)))
 }
